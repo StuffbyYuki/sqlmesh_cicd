7328c1b015cbd107ddcb9dccd67bb1a70d90a5e1
stuffbyyuki <kake.19940627@gmail.com>
2024-12-12 11:36:13 -0700

    modififed polars_model - again - got a column back

--- models/polars_model.py
+++ models/polars_model.py
@@ -13,6 +13,7 @@ from sqlmesh import ExecutionContext, model
         "col_a": "int",
         "col_b": "int",
         "col_c": "boolean",
+        "col_d": "text"
     },
     dialect="duckdb"
 )
@@ -29,7 +30,8 @@ def execute(
     return (
         df
         .with_columns(
-            col_c=pl.col('col_a').eq(1)
+            col_c=pl.col('col_a').eq(1),
+            col_d=pl.lit('column d')
         )
         .to_pandas()
     )
